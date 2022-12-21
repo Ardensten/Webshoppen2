bb142ec7277bea4264e1b400f81120fd22e7131c
friborg <hannafriborg@gmail.com>
2022-12-21 13:59:12 +0100

    Finished input methods, admin and some cleanup and ordering

---
+++ Webshoppen2/Webshoppen2/AllMethods/Admin.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Webshoppen2.Models;
+
+namespace Webshoppen2.AllMethods
+{
+    internal class Admin
+    {
+        internal static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine($"[1] Add a product."
+                + "\n[2] Change a product."
+                + "\n[3] Delete a product."
+                + "\n[4] Add category."
+                + "\n[5] Change customer-info"
+                + "\n[6] View purchase histories");
+
+            ConsoleKeyInfo key = Console.ReadKey(true);
+
+            switch (key.KeyChar)
+            {
+                case '1':
+                    AddProduct();
+                    break;
+                case '2':
+                    EditProduct();
+                    break;
+                case '3':
+                    RemoveProduct();
+                    break;
+                case '4':
+                    AddCategory();
+                    break;
+                case '5':
+                    ChangeCustomerInfo();
+                    break;
+                case '6':
+                    ViewPurchaseHistory();
+                    break;
+                default:
+                    Methods.InputInstructions();
+                    break;
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private static void ViewPurchaseHistory()
+        {
+
+        }
+
+        private static void ChangeCustomerInfo()
+        {
+
+        }
+
+        private static void AddCategory()
+        {
+            Console.WriteLine("Enter catgory name: ");
+            string name = Console.ReadLine();
+            using (var db = new webshoppenContext())
+            {
+                var newCategory = new Category
+                {
+                    Name = name
+                };
+                var categoryList = db.Categories;
+                categoryList.Add(newCategory);
+                db.SaveChanges();
+            }
+        }
+
+        private static void AddProduct()
+        {
+            Console.WriteLine("Enter product name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter product price: ");
+            var price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter product supplier-id: ");
+            int supplierId = Convert.ToInt32(Console.ReadLine());
+            //ShowListSupplier();
+
+            Console.WriteLine("Enter product category-id: ");
+            int categoryId = Convert.ToInt32(Console.ReadLine());
+            //ShowListCategory();
+
+            Console.WriteLine("Enter product information text: ");
+            var infotext = Console.ReadLine();
+            Console.WriteLine("Enter number of products in stock: ");
+            int unitsInStock = Convert.ToInt32(Console.ReadLine());
+
+            using (var db = new webshoppenContext())
+            {
+                var newProduct = new Product
+                {
+                    Name = name,
+                    Price = price,
+                    SupplierId = supplierId,
+                    CategoryId = categoryId,
+                    InfoText = infotext,
+                    UnitsInStock = unitsInStock
+                };
+                var productList = db.Products;
+                productList.Add(newProduct);
+                db.SaveChanges();
+            }
+        }
+
+        public static void RemoveProduct()
+        {
+            Console.WriteLine("Input id of product you want to delete");
+            var productId = Convert.ToInt32(Console.ReadLine());
+
+            using (var db = new webshoppenContext())
+            {
+                var product = db.Products.Where(x => x.Id == productId).SingleOrDefault();
+
+                if (product != null)
+                {
+                    db.Products.Remove((Product)product);
+                    db.SaveChanges();
+                }
+            }
+        }
+
+        public static void EditProduct()
+        {
+            Console.WriteLine("Input id of product you want to edit");
+            var productId = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What do you want to edit?\n" +
+                "[1] : Name\n" +
+                "[2] : Price\n" +
+                "[3] : Infotext\n" +
+                "[4] : Units in Stock\n" +
+                "[5] : Add product to front page\n" +
+                "[6] : Remove product from front page\n" +
+                "[7] : Return to main menu");
+
+            ConsoleKeyInfo key = Console.ReadKey(true);
+            using (var db = new webshoppenContext())
+            {
+                switch (key.KeyChar)
+                {
+                    case '1':
+                        Console.WriteLine("What is the new name");
+                        var newName = Console.ReadLine();
+                        var product = db.Products.Where(x => x.Id == productId).SingleOrDefault();
+
+                        if (product != null)
+                        {
+                            product.Name = newName;
+                            db.SaveChanges();
+                        }
+                        break;
+
+                    case '2':
+                        Console.WriteLine("Enter the updated price: ");
+                        var newPrice = Convert.ToDouble(Console.ReadLine());
+                        product = db.Products.Where(x => x.Id == productId).SingleOrDefault();
+
+                        if (product != null)
+                        {
+                            product.Price = newPrice;
+                            db.SaveChanges();
+                        }
+                        break;
+
+                    case '3':
+                        Console.WriteLine("Enter the updated info-text: ");
+                        var newText = Console.ReadLine();
+                        product = db.Products.Where(x => x.Id == productId).SingleOrDefault();
+
+                        if (product != null)
+                        {
+                            product.InfoText = newText;
+                            db.SaveChanges();
+                        }
+                        break;
+
+                    case '4':
+                        Console.WriteLine("Enter the updated amount of units in stock: ");
+                        var newUnitsInStock = Convert.ToInt32(Console.ReadLine());
+                        product = db.Products.Where(x => x.Id == productId).SingleOrDefault();
+
+                        if (product != null)
+                        {
+                            product.UnitsInStock = newUnitsInStock;
+                            db.SaveChanges();
+                        }
+                        break;
+
+                    case '5':
+                        Console.WriteLine("Added product to front page. ");
+                        product = db.Products.Where(x => x.Id == productId).SingleOrDefault();
+
+                        if (product != null)
+                        {
+                            product.ChosenProduct = true;
+                            db.SaveChanges();
+                        }
+                        break;
+                    case '6':
+                        Console.WriteLine("Removed product to front page. ");
+                        product = db.Products.Where(x => x.Id == productId).SingleOrDefault();
+
+                        if (product != null)
+                        {
+                            product.ChosenProduct = false;
+                            db.SaveChanges();
+                        }
+                        break;
+                    case '7':
+                        Methods.Running();
+                        break;
+
+                    default:
+                        Methods.InputInstructions();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Webshoppen2/Webshoppen2/AllMethods/Methods.cs
@@ -0,0 +1,215 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Webshoppen2.AllMethods;
+
+namespace Webshoppen2.Models
+{
+    internal class Methods
+    {
+        internal static void Running()
+        {
+            Console.Clear();
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("  ____            _                       _                  _    _ _                 _           _                  _   \r\n / ___| _   _ ___| |_ ___ _ __ ___  _   _| |___   _____  ___| | _| (_)_ __   __ _ ___| |__   ___ | | __ _  __ _  ___| |_ \r\n \\___ \\| | | / __| __/ _ \\ '_ ` _ \\| | | | __\\ \\ / / _ \\/ __| |/ / | | '_ \\ / _` / __| '_ \\ / _ \\| |/ _` |/ _` |/ _ \\ __|\r\n  ___) | |_| \\__ \\ ||  __/ | | | | | |_| | |_ \\ V /  __/ (__|   <| | | | | | (_| \\__ \\ |_) | (_) | | (_| | (_| |  __/ |_ \r\n |____/ \\__, |___/\\__\\___|_| |_| |_|\\__,_|\\__| \\_/ \\___|\\___|_|\\_\\_|_|_| |_|\\__, |___/_.__/ \\___/|_|\\__,_|\\__, |\\___|\\__|\r\n        |___/                                                               |___/                         |___/  ");
+                Console.ResetColor();
+                Console.WriteLine($"\n\t\t  Welcome to our beautiful webshop! Here can you buy spirits and get wasted!\n\n"
+                    + "\n1. Free text search."
+                    + "\n2. Categories."
+                    + "\n3. Admin.");
+
+                using(var db = new webshoppenContext())
+                {
+                    foreach (var p in db.Products.Where(p => p.ChosenProduct == true))
+                    {
+                        Console.Write($"{p.Name}\tPrice: {p.Price}SEK");
+                    }
+                }
+
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                switch (key.KeyChar)
+                {
+                    case '1':
+                        //MethodSearch()
+                        break;
+                    case '2':
+                        Categories();
+                        break;
+                    case '3':
+                        Admin.Menu();
+                        break;
+                    default:
+                        InputInstructions();
+                        break;
+                }
+                Console.ReadKey(true);
+                Console.Clear();
+            }
+        }
+
+        internal static void Categories()
+        {
+            Console.Clear();
+            bool runCategories = true;
+
+            while (runCategories)
+            {
+                using (var db = new webshoppenContext())
+                {
+                    foreach (var p in db.Categories)
+                    {
+                        Console.WriteLine($"[{p.Id}] : {p.Name}");
+                    }
+                }
+                Console.WriteLine("Input the category-id you wish to view: ");
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                switch (key.KeyChar)
+                {
+                    case '1':
+                        ShowAllBeer();
+                        Console.WriteLine("Choose a beer: ");
+                        CartChoice();
+                        break;
+                    case '2':
+                        ShowAllWine();
+                        Console.WriteLine("Choose a wine: ");
+                        CartChoice();
+                        break;
+                    case '3':
+                        ShowAllSpirits();
+                        Console.WriteLine("Choose spirits: ");
+                        CartChoice();
+                        break;
+                    case '4':
+                        ShowAllChampagne();
+                        Console.WriteLine("Choose a champagne: ");
+                        CartChoice();
+                        break;
+                    case '5':
+                        ShowAllCider();
+                        Console.WriteLine("Choose a cider: ");
+                        CartChoice();
+                        break;
+                    default:
+                        InputInstructions();
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        private static void ShowAllChampagne()  //slå ihop alla metoder och ta categori id som en inparameter!!!!!!!!!!!!!!!!!!
+        {
+            using (var db = new webshoppenContext())
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"\nList of all champagne. Enjoy!");
+                Console.ResetColor();
+                foreach (var p in db.Products.Where(x => x.CategoryId == 4).Include(s => s.Supplier))
+                {
+                    var inStock = p.UnitsInStock > 0 ? "In stock." : "Out of stock.";
+                    Console.WriteLine($" - [ID: {p.Id}] {p.Name}\tPrice: {p.Price}SEK \t\t{inStock}\t{p.UnitsInStock}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void ShowAllCider()
+        {
+            using (var db = new webshoppenContext())
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"\nList of all ciders. Enjoy!");
+                Console.ResetColor();
+                foreach (var p in db.Products.Where(x => x.CategoryId == 5).Include(s => s.Supplier))
+                {
+                    var inStock = p.UnitsInStock > 0 ? "In stock." : "Out of stock.";
+                    Console.WriteLine($" - [ID: {p.Id}] {p.Name}\tPrice: {p.Price}SEK \t\t{inStock}\t{p.UnitsInStock}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void ShowAllSpirits()
+        {
+            using (var db = new webshoppenContext())
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"\nList of all spirits. Enjoy!");
+                Console.ResetColor();
+                foreach (var p in db.Products.Where(x => x.CategoryId == 3).Include(s => s.Supplier))
+                {
+                    var inStock = p.UnitsInStock > 0 ? "In stock." : "Out of stock.";
+                    Console.WriteLine($" - [ID: {p.Id}] {p.Name}\tPrice: {p.Price}SEK \t\t{inStock}\t{p.UnitsInStock}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void ShowAllBeer()
+        {
+            using (var db = new webshoppenContext())
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"\nList of beers. Enjoy!");
+                Console.ResetColor();
+                foreach (var p in db.Products.Where(x => x.CategoryId == 1).Include(s => s.Supplier))
+                {
+                    var inStock = p.UnitsInStock > 0 ? "In stock." : "Out of stock.";
+                    Console.WriteLine($" - [ID: {p.Id}] {p.Name}\tPrice: {p.Price}SEK \t\t{inStock}\t{p.UnitsInStock}");
+                }
+                Console.WriteLine();
+            }
+        }
+        private static void ShowAllWine()
+        {
+            using (var db = new webshoppenContext())
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"\nList of all wine. Enjoy!");
+                Console.ResetColor();
+                foreach (var p in db.Products.Where(x => x.CategoryId == 2).Include(s => s.Supplier))
+                {
+                    var inStock = p.UnitsInStock > 0 ? "In stock." : "Out of stock.";
+                    Console.WriteLine($" - [ID: {p.Id}] {p.Name}\tPrice: {p.Price}SEK \t\t{inStock}\t{p.UnitsInStock}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        internal static void CartChoice()
+        {
+
+            Console.WriteLine("Add to cart?");
+            var choice = Console.ReadLine();
+            if (choice == "y")
+            {
+                //AddProductToCart();
+            }
+            else if (choice == "n")
+            {
+                Categories();
+            }
+            else
+            {
+                InputInstructions();
+            }
+        }
+
+        internal static void InputInstructions()
+        {
+            Console.WriteLine("Wrong input. Try something else!");
+        }
+    }
+}
