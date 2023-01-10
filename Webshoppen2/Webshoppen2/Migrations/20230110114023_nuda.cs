86f822c6110b7606c8d544f8f8d5dd81e10860e2
Dantrew <115991743+Dantrew@users.noreply.github.com>
2023-01-10 12:41:54 +0100

    updatade db
    updatade db

---
+++ Webshoppen2/Webshoppen2/Migrations/20230110114023_nuda.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Webshoppen2.Migrations
+{
+    public partial class nuda : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+name: "CheckoutCart",
+columns: table => new
+{
+    Id = table.Column<int>(type: "int", nullable: false)
+.Annotation("SqlServer:Identity", "1, 1"),
+    AmountofUnits = table.Column<int>(type: "int", nullable: true),
+    CustomerId = table.Column<int>(type: "int", nullable: true),
+    OrderId = table.Column<int>(type: "int", nullable: false),
+    ProductId = table.Column<int>(type: "int", nullable: true)
+},
+constraints: table =>
+{
+    table.PrimaryKey("PK_CheckoutCart", x => x.Id);
+});
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
